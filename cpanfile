5f187b0cb93ec65d008c46898e93c590e6ec970e
Michael Jemmeson <mjemmeson@cpan.org>
2018-01-23 16:01:02 +0000

    prepare for build

---
+++ cpanfile
@@ -0,0 +1,3 @@
+requires 'Test::More'  => 0;
+requires 'Time::HiRes' => 0;
+
--- lib/Test/Time/HiRes.pm
+++ lib/Test/Time/HiRes.pm
@@ -7,7 +7,7 @@ use Test::More;
 use Test::Time;
 use Time::HiRes ();
 
-our $VERSION      = '0.01';
+our $VERSION = '0.01';
 
 our $time;    # epoch in microseconds
 our $seconds;         # i.e. standard epoch
@@ -78,7 +78,7 @@ sub import {
             return 0 unless $sleep;
 
             $time    = _time() + $sleep;
-            $seconds = int($time / 1_000_000 );
+            $seconds = int( $time / 1_000_000 );
 
             # update Test::Time to keep our $time's in sync
             if ( $seconds > $Test::Time::time ) {
@@ -107,4 +107,70 @@ sub import {
 1;
 
 __END__
+
+=head1 NAME
+
+Test::Time::HiRes - drop-in replacement for Test::Time to work with Time::HiRes
+
+=head1 SYNOPSIS
+
+    use Test::Time::HiRes time => 123.456789;
+
+    # Freeze time
+    my $now       = time();
+    my $now_hires = Time::HiRes::time();
+
+    # Increment internal time (returns immediately)
+    sleep 1;        # seconds
+    usleep 1000;    # microseconds
+
+    # Return internal time incremented by 1.001 s
+    my $then       = time();
+    my $then_hires = Time::HiRes::time();
+
+=head1 DESCRIPTION
+
+Drop-in replacement for L<Test::Time> that also works with the L<Time::HiRes>
+functions C<usleep> and C<gettimeofday>.
+
+=head1 SEE ALSO
+
+=over
+
+=item L<Test::Time>
+
+=back
+
+=head1 SUPPORT
+
+=head2 Bugs / Feature Requests
+
+Please report any bugs or feature requests through the issue tracker
+at L<https://github.com/mjemmeson/Test-Time-HiRes/issues>.
+You will be notified automatically of any progress on your issue.
+
+=head2 Source Code
+
+This is open source software.  The code repository is available for
+public review and contribution under the terms of the license.
+
+L<https://github.com/mjemmeson/Test-Time-HiRes>
+
+    git clone git://github.com/mjemmeson/Test-Time-HiRes.git
+
+=head1 AUTHOR
+
+Michael Jemmeson E<lt>mjemmeson@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2018- Michael Jemmeson
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
 
